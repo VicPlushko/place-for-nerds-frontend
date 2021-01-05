ec3ffb8aebbf54c25b276033c4d72bddc7a2f7cb
github username <github email address>
2021-01-05 12:57:34 -0500

    changes App.js to class component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Movies</h1>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Movies</h1>
+        </header>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
