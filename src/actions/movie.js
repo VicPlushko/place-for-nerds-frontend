4f933b6bf3306f3fa7af9242066b520111642020
github username <github email address>
2021-01-06 13:03:50 -0500

    successfully makes first fetch

--- src/actions/movie.js
+++ src/actions/movie.js
@@ -6,6 +6,6 @@ export const getMovies = () => {
         dispatch({type: "LOADING_MOVIES"})
         fetch(URL)
         .then(resp => resp.json())
-        .then(movies => console.log(movies.results.title))
+        .then(movie => dispatch({type: 'MOVIES_LOADED', payload: movie.results}))
     }
 }
