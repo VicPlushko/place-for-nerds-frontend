ee1bd683c246c029f5216741dcb1a5a0fd1a7026
github username <github email address>
2021-01-18 03:23:30 -0500

    adds styling and display change to movies cast

--- src/components/Actor.js
+++ src/components/Actor.js
@@ -10,6 +10,7 @@ const Movie = (props) => {
               <div className='poster-link'><Link to={`/actors/${props.id}`}><img src={imageURL + props.profile_path} alt="" id={props.id}></img></Link>
                 <div className='movie-title'>
                   <Link className='title-link'to={`/actors/${props.id}`}>{props.name}</Link>
+                <div className='cast-character'>{props.character}</div>
                 </div>
               </div>
             </div>
