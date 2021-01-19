1dc386617abdb590c784930512572e1e3ba29b0e
github username <github email address>
2021-01-19 16:37:13 -0500

    adds tvCredits component

--- src/containers/ActorContainer.js
+++ src/containers/ActorContainer.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import Navigation from '../components/Navigation'
 import SingleActor from '../components/SingleActor'
 import MovieCredits from '../components/MovieCredits'
+import TvCredits from '../components/TvCredits'
 
 
 class ActorContainer extends Component {
@@ -22,7 +23,6 @@ class ActorContainer extends Component {
 
     componentDidMount() {
         const MOVIES_KEY = process.env.REACT_APP_MOVIES_KEY
-        const actorPic = 'http://image.tmdb.org/t/p/w185'
         // console.log('actor container props is', this.props)
         const actorUrl = `https://api.themoviedb.org/3/person/${this.props.match.params.id}?api_key=${MOVIES_KEY}&language=en-US&append_to_response=movie_credits,tv_credits`
         fetch(actorUrl)
@@ -41,16 +41,9 @@ class ActorContainer extends Component {
                 movieCredits: data.movie_credits.cast.map((movie, i) => {
                     return <MovieCredits key={i} movie_id={movie.id} poster={movie.poster_path} title={movie.title} release_date={movie.release_date} character={movie.character} />
                 }),
-                    // <div className='castGrid'>
-                    //     <Link className='poster-link' to={`/movies/${movie.id}`}><img className='movie-poster' src={actorPic + movie.poster_path} alt=""></img></Link>
-                    // <div className='movie-name'>{movie.title} - {getYear(`${movie.release_date}`)}</div>
-                    // <div className='character-name'>{movie.character}</div>
-                    // </div>),
-                tvCredits: data.tv_credits.cast.map(show => 
-                    <div className='castGrid'><Link className='poster-link'><img className='show-poster' src={actorPic + show.poster_path} alt=""></img></Link>
-                    <div className='movie-name'>{show.name} - {getYear(`${show.first_air_date}`)}</div>
-                    <div className='character-name'>{show.character}</div>
-                    </div>)
+                tvCredits: data.tv_credits.cast.map((show, i) => {
+                    return <TvCredits key={i} show_id={show.id} poster={show.poster_path} title={show.name} release_date={show.first_air_date} character={show.character} />
+                }), 
             })
         })
         
@@ -76,9 +69,4 @@ class ActorContainer extends Component {
         )
     }
 }
-
-const getYear = (x) => {
-   return x.split('-')[0]
-}
-
 export default ActorContainer
