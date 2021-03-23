514fe9746069eebcb40d4b2364588eea27dd296c
Victor Plushko <vicplushko33@gmail.com>
2021-03-23 13:30:23 -0400

    adds email to initial state, reformats case statement.

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,7 +1,11 @@
-const userReducer = (state = {username: "", password: ""}, action) => {
+const userReducer = (state = {username: "", password: "", email: ""}, action) => {
     switch(action.type) {
         case("CREATE_USER"):
-            return {...state, username: action.payload.username, password: action.payload.password}
+            return {...state, 
+                username: action.payload.username, 
+                password: action.payload.password,
+                email: action.payload.email
+            }
         default:
             return state
     }
