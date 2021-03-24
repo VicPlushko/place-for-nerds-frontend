7300ff2091f52c58b834b04603156b21ac6f28b0
Victor Plushko <vicplushko33@gmail.com>
2021-03-24 02:45:35 -0400

    adds styling and floating labels for form fields. and adds labels to sign up form.

--- src/components/user/SignupForm.js
+++ src/components/user/SignupForm.js
@@ -7,19 +7,24 @@ const SignupForm = (props) => {
             <h1>Sign Up</h1>
             <form className='user-form' onSubmit={props.handleCreateUser}>
                 <div className='form-field'>
-                    <input onChange={props.changeUsername} type='text' name='username' placeholder='Username'/>
+                    <input className='form-input' onChange={props.changeUsername} type='text' name='username' placeholder='Username'/>
+                    <label className='form-label'>Hello, My Name Is</label>
                 </div>
                 <div className='form-field'>
-                    <input onChange={props.changeEmail} type='text' name='email' placeholder='Email'/>
+                    <input className='form-input' onChange={props.changeEmail} type='text' name='email' placeholder='Email'/>
+                    <label className='form-label'>Email</label>
                 </div>
                 <div className='form-field'>
-                    <input onChange={props.changeEmailConfirm} type='text' name='emailConfirm' placeholder='Confirm Email'/>
+                    <input className='form-input' onChange={props.changeEmailConfirm} type='text' name='emailConfirm' placeholder='Confirm Email'/>
+                    <label className='form-label'>Confirm Email</label>
                 </div>
                 <div className='form-field'>
-                    <input onChange={props.changePassword} type='password' name='password' placeholder='Password'/>
+                    <input className='form-input' onChange={props.changePassword} type='password' name='password' placeholder='Password'/>
+                    <label className='form-label'>Password</label>
                 </div>
                 <div className='form-field'>
-                    <input onChange={props.changePasswordConfirm} type='password' name='passwordConfirm' placeholder='Confirm Password'/>
+                    <input className='form-input' onChange={props.changePasswordConfirm} type='password' name='passwordConfirm' placeholder='Confirm Password'/>
+                    <label className='form-label'>Confirm Password</label>
                 </div>
                 <div className='btn-submit'>
                     <input className='user-submit-btn' type='submit' value='Create Account'/>
