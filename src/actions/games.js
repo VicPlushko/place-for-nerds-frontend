1df1833d1fc8fb0a7edfd0aff6d8a3d07b0b5222
Victor Plushko <vicplushko33@gmail.com>
2021-05-19 23:36:46 -0400

    Deletes files

--- src/actions/games.js
+++
@@ -1,4 +0,0 @@
-export const getGames = () => {
-    const GAMES_KEY = process.env.REACT_APP_GAMES_KEY
-    const gamesURL = `https://api.rawg.io/api/games?key=${GAMES_KEY}&page_size=30&dates=2019-01-01,2021-03-30&ordering=-metacritic&metacritic=80,100`
-}
