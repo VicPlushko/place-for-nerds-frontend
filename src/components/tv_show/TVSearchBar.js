bc522471ad70ad4ae2306b4cf3ce3b0a6e734b7f
Victor Plushko <vicplushko33@gmail.com>
2021-06-30 12:26:21 -0400

    Styles the tv show page

--- src/components/tv_show/TVSearchBar.js
+++ src/components/tv_show/TVSearchBar.js
@@ -9,11 +9,13 @@ const TVSearchBar = (props) => {
 
     return (
         <div className='search-form'>
-        <form className='search-bar' onSubmit={handleSubmit}>
-            <input  id='search-input' onChange={handleChange} type='text' placeholder='Search TV Shows' />
-            <input id='submit-btn' type='submit' value='Search'/>
-        </form>
-    </div>
+            <form className='search-bar' onSubmit={handleSubmit}>
+                <input  id='search-input' onChange={handleChange} type='text' placeholder='Search TV Shows' />
+                <div className='submit-btn-div'>
+                    <input id='submit-btn' type='submit' value='Search'/>
+                </div>
+            </form>
+        </div>
     )
 }
 
--- src/containers/tv_shows/TvShowContainer.js
+++ src/containers/tv_shows/TvShowContainer.js
@@ -21,8 +21,8 @@ class TvShowContainer extends Component {
             return <TvShow key={i}  title={show.name} release_date={show.first_air_date} synopsis={show.overview} poster={show.poster_path} show_id={show.id}/>
             })
         return (
-            <div>
-                <div className='App-header'>
+            <div className="movie-outer-div">
+                <div className='movies-header'>
                     <h1>TV Shows</h1>   
                 </div>
                 <div className='search-div'>
