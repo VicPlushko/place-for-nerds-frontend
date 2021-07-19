cfe78bca26d19c9a03b65c25ed775127150783bf
Victor Plushko <vicplushko33@gmail.com>
2021-07-19 14:28:10 -0400

    changed Navigation to a class component and connected to redux store, added conditional rendering of user buttons and links. added some styling

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,17 @@
-import React, { useState }from 'react'
+import React, { Component }from 'react'
+import { connect } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 import LogoutButton from '../components/user/LogoutButton'
 
-const Navigation = () => {
-    
-    const [isAuthenticated] = useState(localStorage.getItem('token') ? true : false)
+class Navigation extends Component {
 
-    const handleLogoutOnButtonClick = (event) => {
+    render() {
+
+      const {
+        isAuthenticated
+      } = this.props
+
+      const handleLogoutOnButtonClick = (event) => {
         console.log(event.target)
         if (isAuthenticated === true) {
           localStorage.removeItem('token')
@@ -27,20 +32,33 @@ const Navigation = () => {
     
     const userLinks = {
         padding: '2px',
-        margin: '10px 5px'
+        margin: '10px 5px',
+        textDecoration: 'none',
+        color: '#069'
     }
-
-    return (
-        <div className='Navbar'>
-           <NavLink to='/' exact style={links} activeStyle={{background: 'red'}}>Home</NavLink>
-           <NavLink to='/movies' exact style={links} activeStyle={{background: 'red'}}>Movies</NavLink>
-           <NavLink to='/tv_shows' exact style={links} activeStyle={{background: 'red'}}>TV Shows</NavLink>
-           <NavLink to='/register' exact style={userLinks}>Sign up</NavLink>
-           <NavLink to='/login' exact style={userLinks}>Login</NavLink>
-           <LogoutButton handleLogout={handleLogoutOnButtonClick}/>
-           
-        </div>
-    )
+      return (
+          <div className='Navbar'>
+            <NavLink to='/' exact style={links} activeStyle={{background: 'red'}}>Home</NavLink>
+            <NavLink to='/movies' exact style={links} activeStyle={{background: 'red'}}>Movies</  NavLink>
+            <NavLink to='/tv_shows' exact style={links} activeStyle={{background: 'red'}}>TV Shows</NavLink>
+            {!isAuthenticated ?
+              <>          
+                <NavLink to='/register' exact style={userLinks}>Sign up</NavLink>
+                <NavLink to='/login' exact style={userLinks}>Login</NavLink>
+              </>  
+                : ""
+            }
+            
+            {isAuthenticated ? <LogoutButton handleLogout={handleLogoutOnButtonClick}/> : ""}
+          </div>
+    )}
 }
 
-export default Navigation
+const mapStateToProps = globalState => {
+  console.log("Nav Props is", globalState.userReducer.isAuthenticated)
+  return {
+    isAuthenticated: globalState.userReducer.isAuthenticated
+  }
+} 
+
+export default connect(mapStateToProps)(Navigation) 
