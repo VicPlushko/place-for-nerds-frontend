3bb4b5a1cefaed830f47296f05294ec3d390d3f4
Victor Plushko <vicplushko33@gmail.com>
2021-07-26 01:33:45 -0400

    adds empty field error messages to login and registration

--- src/containers/user/Login.js
+++ src/containers/user/Login.js
@@ -9,61 +9,64 @@ class Login extends Component {
 
     handleChangeUsername = (event) => {
         const {changeUsername} = this.props
-        console.log('login username is', event.target.value)
         changeUsername(event.target.value)
     }
 
     handleChangePassword = (event) => {
         const {changePassword} = this.props
-        console.log('login password is', event.target.value)
         changePassword(event.target.value)
     }
     
     handleLoginUser = (event) => {
 
         const {
+            username,
+            password,
             startUserLogin,
             userLogin,
             userLoginFail
         } = this.props
 
-        startUserLogin()
-        event.preventDefault()
-        const loginUrl = 'http://localhost:3001/login'
-        let loginBody = {
-            username: event.target.elements.username.value,
-            password: event.target.elements.passwordField.value
-        }
-        console.log("login details", loginBody)
-        fetch(loginUrl, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json"
-            },
-            body: JSON.stringify(loginBody)
-        })
-        .then(resp => resp.json())
-        .then(data =>  {
-            console.log(data)
-            if (data.failure !== undefined) {
-                console.log("failed to login: ", data)
-            }else{
-                if (data.jwt !== undefined) {
-                    console.log("jwt exists: ", data)
-                    localStorage.setItem("token", data.jwt)
-                    userLogin(data)
-                    this.props.history.goBack()
-                } else {
-                    console.log("failed to login: ", data)
-                    userLoginFail(alert)
-                }
+        if (username.length === 0) {
+            window.alert('Enter your username to continue login.')
+            event.preventDefault()
+        } else if (password.length === 0) {
+            window.alert('Please enter your password to continue login')
+            event.preventDefault()
+        } else {
+            startUserLogin()
+            event.preventDefault()
+            const loginUrl = 'http://localhost:3001/login'
+            let loginBody = {
+                username: event.target.elements.username.value,
+                password: event.target.elements.passwordField.value
             }
-        })
+            fetch(loginUrl, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json"
+                },
+                body: JSON.stringify(loginBody)
+            })
+            .then(resp => resp.json())
+            .then(data =>  {
+                if (data.failure !== undefined) {
+                    window.alert('failed to login please try again')
+                }else{
+                    if (data.jwt !== undefined) {
+                        localStorage.setItem("token", data.jwt)
+                        userLogin(data)
+                        this.props.history.goBack()
+                    } else {
+                        userLoginFail(alert)
+                    }
+                }
+            })
+        }
     };
     
     render() {
-        console.log("login state is", this.props)
         return (
             <div>
                 <LoginForm handleSubmit={this.handleLoginUser} changeUsername={this.handleChangeUsername} changePassword={this.handleChangePassword} />
--- src/containers/user/Registration.js
+++ src/containers/user/Registration.js
@@ -3,73 +3,87 @@ import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import * as userActions from '../../actions/user'
 import RegistrationForm from '../../components/user/RegistrationForm'
-// import { Redirect } from 'react-router-dom'
 
 class Signup extends Component {
 
     handleChangeUsername = (event) => {
         const {changeUsername} = this.props
-        console.log("username is", event.target.value)
         changeUsername(event.target.value)
     }
 
     handleChangeEmail = (event) => {
         const {changeEmail} = this.props
-        console.log("email is", event.target.value)
         changeEmail(event.target.value)
     }
 
     handleChangeEmailConfirm = (event) => {
         const {changeEmailConfirm} = this.props
-        console.log("email confirm is", event.target.value)
         changeEmailConfirm(event.target.value)
     }
 
     handleChangePassword = (event) => {
         const {changePassword} = this.props
-        console.log("password is", event.target.value)
         changePassword(event.target.value)
     }
 
     handleChangePasswordConfirm = (event) => {
         const {changePasswordConfirm} = this.props
-        console.log("password confirm is", event.target.value)
         changePasswordConfirm(event.target.value)
     }
 
     handleOnSubmit = (event) => {
 
         const {
+            username,
+            email,
+            emailConfirm,
             passwordConfirm,
             password,
             createUser
         } = this.props
 
-        const userInfo = {
-            username: event.target.elements.username.value,
-            email: event.target.elements.email.value,
-            password: event.target.elements.passwordField.value,
-        }
-        event.preventDefault()
-        const URL = 'http://localhost:3001/register'
-        if (passwordConfirm === password ) {
-            fetch(URL, {
-                method: "POST",
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(userInfo)
-            })
-            .then(resp => resp.json())
-            .then(data => {
-                console.log(data)
-                createUser(data)
-                this.props.history.push("/login")
-            })
-        }else {
-            alert("Email or passwords do not match")
-        }
+        if (username.length === 0) {
+            window.alert('Username can not be empty.')
+            event.preventDefault()
+        } else if (email.length === 0) {
+            window.alert('Email can not be empty.')
+            event.preventDefault()
+        } else if (emailConfirm.length === 0) {
+            window.alert('Confirm email can not be empty.')
+            event.preventDefault()
+        } else if (password === 0) {
+            window.alert('Password can not be empty.')
+            event.preventDefault()
+        } else if (passwordConfirm.length === 0) {
+            window.alert('Confirm password can not be empty.')
+            event.preventDefault()
+        } else {
+            const userInfo = {
+                username: event.target.elements.username.value,
+                email: event.target.elements.email.value,
+                password: event.target.elements.passwordField.value,
+            }
+            event.preventDefault()
+            const URL = 'http://localhost:3001/register'
+            if (passwordConfirm === password ) {
+                fetch(URL, {
+                    method: "POST",
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(userInfo)
+                })
+                .then(resp => resp.json())
+                .then(data => {
+                    console.log(data)
+                    createUser(data)
+                    this.props.history.push("/")
+                })
+            }else {
+                alert("Email or passwords do not match")
+            }
+        }        
     }
 
     render() {
@@ -82,11 +96,10 @@ class Signup extends Component {
 }
 
 const mapStateToProps = globalState => {
-    console.log("global state is", globalState)
     return {
         username: globalState.userReducer.username,
         email: globalState.userReducer.email,
-        emailComfirm: globalState.userReducer.emailComfirm,
+        emailConfirm: globalState.userReducer.emailConfirm,
         password: globalState.userReducer.password,
         passwordConfirm: globalState.userReducer.passwordConfirm
     }
